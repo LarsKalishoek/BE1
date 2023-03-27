c4a775027e842b918b499d7f805cd0166d870064
LarsKalishoek <99069312@mydavinci.nl>
2023-03-27 19:07:23 +0200

    begin van goedemorge opdracht gemaakt

--- goedemorgen.php
+++ goedemorgen.php
@@ -1,1 +1,24 @@
 <?php
+// set the default timezone to use.
+date_default_timezone_set('Europe/Amsterdam');
+$time = date("h:i A");
+switch ($time) {
+    
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Goede Dag</title>
+    <!-- CSS -->
+    <style>
+
+    </style>
+   <!-- HTML --> 
+  </head>
+  <body>
+    <div class = "time"></div>
+  </body>
+</html>
